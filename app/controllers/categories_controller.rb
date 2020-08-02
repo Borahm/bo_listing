41739d76d32c5572a47c80d2a28c49b5081e2131
borahm <borahm@borahm.de>
2020-08-02 00:22:07 +0000

    adding pages/links/views to categories and subcategories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,13 @@ class CategoriesController < ApplicationController
         @categories = Category.all
         @community = @categories[0]
         @housing = @categories[1]
+        @jobs = @categories[2]
+        @personals = @categories[3]
+        @sale = @categories[4]
+        @services = @categories[5]
+    end
 
-
+    def show
+        
     end
 end
